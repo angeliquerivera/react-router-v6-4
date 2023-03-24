4bddee133655d480860654e71f3eb50ced904c33
Angelique Rivera <angeliqueriveramorales@gmail.com>
2023-03-24 13:11:39 -0400

    add redirect to edit page

--- src/routes/edit.jsx
+++ src/routes/edit.jsx
@@ -1,4 +1,5 @@
 import { Form, useLoaderData, redirect } from "react-router-dom";
+import { updateContact } from "../contacts";
 
 export async function action({ request, params }) {
   const formData = await request.formData();
